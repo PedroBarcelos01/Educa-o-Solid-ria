d8bf245beff15f2ffb31064b45aa8dfd393b27e8
Pedro Barcelos <pedromiguel1203@hotmail.com>
2024-11-12 17:06:02 -0300

    Add files via upload

--- doacao.js
+++ doacao.js
@@ -1,3 +1,16 @@
+function mascara(valor) {
+	var valorAlterado = valor.value;
+        if (valorAlterado === '') {
+            valor.value = '';
+            return;
+        }
+        valorAlterado = valorAlterado.replace(/\D/g, "");
+        valorAlterado = valorAlterado.replace(/(\d+)(\d{2})$/, "$1,$2");
+        valorAlterado = valorAlterado.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+        valorAlterado = "R$ " + valorAlterado;
+        valor.value = valorAlterado;
+}
+
 const wrapper =document.querySelector(".wrapper"),
 qrInput = wrapper.querySelector(".form input"),
 generateBtn =wrapper.querySelector(".form button"),
@@ -6,8 +19,10 @@ qrImg = wrapper.querySelector(".qr-code img");
 generateBtn.addEventListener("click", () => {
     let qrValue = qrInput.value;
     if(!qrValue) return;
+    const customMessage = "Pagamento efetuado no valor de: ";
+    const qrData = customMessage + qrValue;
     generateBtn.innerText = "Gerando um Qr Code..."
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${encodeURIComponent(qrData)}`;
     qrImg.addEventListener("load", () => {
         generateBtn.innerText = "Gerar Qr Code"
         wrapper.classList.add("active");
